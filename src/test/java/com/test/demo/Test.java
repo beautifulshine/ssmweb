606327ac35015de1da09e384f367d5925cedfff6
liqin <1179139256@qq.com>
2017-11-08 09:18:33 +0800

    20171108 ssm

--- src/test/java/com/test/demo/Test.java
+++ src/test/java/com/test/demo/Test.java
@@ -1,5 +1,12 @@
 package com.test.demo;
 
+import net.sf.json.JSONObject;
+
 public class Test {
+	public static void main(String[] args) {
+		String s = "{\"name\":\"vox\",\"age\":\"15\"}";
+		 JSONObject jsonObject=JSONObject.fromObject(s);
+		System.out.println(jsonObject.getString("name"));
+	}
 
 }
