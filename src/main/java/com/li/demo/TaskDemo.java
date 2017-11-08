542795a6c4d61fea60282c5bc30d0060f199d2fe
liqin <1179139256@qq.com>
2017-11-08 17:22:09 +0800

    spring scheduled

---
+++ src/main/java/com/li/demo/TaskDemo.java
@@ -0,0 +1,25 @@
+package com.li.demo;
+
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+public class TaskDemo {
+	
+	@Scheduled(cron = "0 34 16 * * ?")
+	
+	public static  void doSomething() { 
+	   System.out.println("每五秒钟执行一次cron");
+	}
+	
+	
+	@Scheduled(fixedDelay = 10000)
+	public void doSomething2() { 
+		  System.out.println("每五秒钟执行一次2fixedDelay");
+	}
+	
+	@Scheduled(fixedRate = 10000)
+	public void doSomething3() { 
+		 System.out.println("每五秒钟执行一次3fixedRate");
+	}
+}
